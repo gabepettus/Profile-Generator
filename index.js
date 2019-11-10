9ff0ef8e77931e77ffb42c18e7c79546bedcc564
gdpettus <gabe.pettus@gmail.com>
2019-11-09 21:23:42 -0700

    added inquirer, chained axios

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
+const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
 
-const appendFileAsync = util.promisify(fs.appendFile);
-const readFileAsync = util.promisify(fs.readFile);
+inquirer.prompt([
+  {
+    type: "input",
+    name: "username",
+    message: "what user name would like to generate a profile for?"
+  },
+  {
+    type: "list",
+    message: "What is your favorite color?",
+    name: "contact",
+    choices: [
+      "green",
+      "blue",
+      "pink"
+    ]
+  }
+]).then(response => {
+  console.log("Got user response", response);
+  const userid = response.username;
+  const color = response.color;
+  // now pull data from axios
 
 const config = { headers: { accept: "application/json" } };
-
-axios.get("https://api.github.com/users/gabepettus", config)
+axios.get(`https://api.github.com/users/${userid}`, config)
   .then(function(res) {
 
-    console.log(res.data);
+    // console.log("output",res.data);
+    return(res.data);
+
+  }).then(function(rrr) {
+    console.log("output promise",rrr);
   });
+
+});
